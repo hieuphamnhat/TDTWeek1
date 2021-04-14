b99bd628b5c4d8f4a9753a819a3e8863628df6c6
Pham Nhat Hieu <44352724+lev3l@users.noreply.github.com>
2021-04-14 10:36:01 +0700

    create linked list python

---
+++ LinkedList.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+    def Atbegining(self, data_in):
+        NewNode = Node(data_in)
+        NewNode.next = self.head
+        self.head = NewNode
+#Inserting in between two Data Nodes
+def Inbetween(self,middle_node,newdata):
+        if middle_node is None:
+            print("The mentioned node is absent")
+            return
+
+        NewNode = Node(newdata)
+        NewNode.nextval = middle_node.nextval
+        middle_node.nextval = NewNode
+
+# Print the linked list
+def listprint(self):
+    printval = self.headval
+    while printval is not None:
+        print (printval.dataval)
+        printval = printval.nextval
+ def RemoveNode(self, Removekey):
+        HeadVal = self.head
+        if (HeadVal is not None):
+            if (HeadVal.data == Removekey):
+                self.head = HeadVal.next
+                HeadVal = None
+                return
+        while (HeadVal is not None):
+            if HeadVal.data == Removekey:
+                break
+            prev = HeadVal
+            HeadVal = HeadVal.next
+        if (HeadVal == None):
+            return
+
+        prev.next = HeadVal.next
+
+        HeadVal = None
+            
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
